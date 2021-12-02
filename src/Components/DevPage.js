2041088e5328f38a517c8dd9b1817a3623b98c2e
dtcaro96 <dtcaro96@gmail.com>
2021-12-02 14:00:23 -0500

    make it better

--- src/Components/DevPage.js
+++ src/Components/DevPage.js
@@ -26,7 +26,7 @@ export default function DevPage () {
                         As soon as I left, I started diving into a few online courses where I got to get my hands dirty with basic HTML, CSS, and Javascript, your basic web dev tools. Eventually when I felt comfortable with the basics I moved into some more complex frameworks and tools including React, Axios, API's, Material-UI, Bootstrap, MongoDB and also some backend technologies including REST concepts, Webpack, NodeJS and Express. I still continue to use my time to learn more and build upon the basic foundations of what I've learned so far.
                         <br />
                         <br />
-                        As I've been learning, I've been working on projects of my own including this website, which is completely responsive! I absolutely love development and I'm super excited to break into the industry myself and learn more about the behind-the-scenes of software engineering. I can't wait to share more as I progress :)
+                        As I've been learning, I've been working on projects of my own including this website, which is completely responsive! I absolutely love development and I'm super excited to break into the industry myself and learn more about the behind-the-scenes of software engineering. I can't wait to share more as I progress :) .
 
                     </p>
 
