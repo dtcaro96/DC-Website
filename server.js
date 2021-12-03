d2480096105d7e441a1acbb86fd68dabb371934c
dtcaro96 <dtcaro96@gmail.com>
2021-12-03 12:59:12 -0500

    add app.get line to server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'build', 'index.html'))
+})
+
 const server = app.listen(app.get('port'), function () {
     console.log('listening on port ', server.address().port);
 });
